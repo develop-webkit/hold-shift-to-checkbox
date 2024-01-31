f7631345e35cf99cabff3d9c55226727475c892e
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2024-02-01 00:27:36 +0500

    2.1

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -1,1 +1,31 @@
 "use strict;"
+
+const inputs = document.querySelectorAll('.inbox input[type="checkbox"]');
+const uncheck = document.querySelector(".check")
+let lastCheck;
+let inBetween = false;
+
+function displayChange(e){
+    console.log(e);
+    if(e.altKey){
+        for(const input of inputs){
+            if(input === this || input === lastCheck){
+                inBetween = !inBetween;
+            }
+            if(inBetween){
+                input.checked = true;
+            }
+        }
+    }
+    lastCheck = this;
+}
+
+for(const input of inputs){
+    input.addEventListener('click',displayChange);
+}
+
+uncheck.addEventListener('click',() => {
+    for(const input of inputs){
+        input.checked = false;
+    }
+});
